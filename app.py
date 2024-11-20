219b45cd5467cb70d7c57458f4a3950d12570a5d
Jiun Wu <jiun.wu@icloud.com>
2024-11-20 11:04:06 +0100

    es

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 @app.route("/")
-def news():
- headlines = scrape_news()
- return render_template("news.html", headlines=headlines)
+def home():
+ app.logger.info("run the landing page")   
+ titles = scrape_airbnb()
+ return render_template("news.html", headlines=titles)
 
 
 @app.route('/hello/')
@@ -24,3 +25,12 @@ def scrape_news():
  for headline in soup.find_all("span", class_="titleline"):
  	headlines.append(headline.text)	
  return headlines
+
+def scrape_airbnb():
+ app.logger.debug("enter the function")
+ r=requests.get('https://en.comparis.ch/immobilien/marktplatz/lugano/moebilierte-wohnung/mieten')
+ soup = BeautifulSoup(r.content, "html.parser")
+ titles = []	
+ for i in soup.find_all("h2"):
+    titles.append(i.text)
+ return titles
